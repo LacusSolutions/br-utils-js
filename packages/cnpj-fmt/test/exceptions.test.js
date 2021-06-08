d304fddd3cc6eb2273d2519941a1ce82fb781414
Julio Muller <juliodolago@gmail.com>
2021-06-07 23:52:57 -0300

    Add tests

---
+++ packages/cnpj-fmt/test/exceptions.test.js
@@ -0,0 +1,38 @@
+const cnpjFmt = require('../build');
+
+test('Option with range start -1 throws TypeError', () => {
+  const cnpj = () => cnpjFmt('03603568000195', {
+    hidden: true,
+    hiddenRange: { start: -1 },
+  });
+  expect(cnpj).toThrow(TypeError);
+});
+
+test('Option with range start greater than 13 throws TypeError', () => {
+  const cnpj = () => cnpjFmt('03603568000195', {
+    hidden: true,
+    hiddenRange: { start: 14 },
+  });
+  expect(cnpj).toThrow(TypeError);
+});
+
+test('Option with range end -1 throws TypeError', () => {
+  const cnpj = () => cnpjFmt('03603568000195', {
+    hidden: true,
+    hiddenRange: { end: -1 },
+  });
+  expect(cnpj).toThrow(TypeError);
+});
+
+test('Option with range end greater than 13 throws TypeError', () => {
+  const cnpj = () => cnpjFmt('03603568000195', {
+    hidden: true,
+    hiddenRange: { end: 14 },
+  });
+  expect(cnpj).toThrow(TypeError);
+});
+
+test('Option with onfail as not a function throws TypeError', () => {
+  const cnpj = () => cnpjFmt('03603568000195', { onFail: 'testing' });
+  expect(cnpj).toThrow(TypeError);
+});
