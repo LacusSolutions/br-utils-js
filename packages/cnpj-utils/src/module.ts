e1c376e7dd7df89796fcad2ab981c2add8585805
juliolmuller <juliolmuller@outlook.com>
2025-03-15 02:26:14 -0300

    refactor(utils): lint source code

--- packages/cnpj-utils/src/module.ts
+++ packages/cnpj-utils/src/module.ts
@@ -1,11 +1,11 @@
-import cnpjFmt from '@lacussoft/cnpj-fmt'
-import cnpjGen from '@lacussoft/cnpj-gen'
-import cnpjVal from '@lacussoft/cnpj-val'
+import cnpjFmt from '@lacussoft/cnpj-fmt';
+import cnpjGen from '@lacussoft/cnpj-gen';
+import cnpjVal from '@lacussoft/cnpj-val';
 
-export const format = cnpjFmt
+export const format = cnpjFmt;
 
-export const generate = cnpjGen
+export const generate = cnpjGen;
 
-export const isValid = cnpjVal
+export const isValid = cnpjVal;
 
-export default { format, generate, isValid }
+export default { format, generate, isValid };
