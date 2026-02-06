0da059a60d79778b1f4bca793e228a9afb6af8ec
juliolmuller <juliolmuller@outlook.com>
2026-02-06 16:24:08 -0300

    docs(cnpj-dv): fix typo in JSdocs
    Fix as per CodeRabbit AI review comment at https://github.com/LacusSolutions/br-utils-js/pull/43#discussion_r2775598478.

--- packages/cnpj-dv/src/types.ts
+++ packages/cnpj-dv/src/types.ts
@@ -4,7 +4,7 @@
  * A CNPJ can be provided as:
  *
  * - A string containing alphanumeric characters (with or without formatting)
- * - An array of strings, where each string represents a alphanumeric character or
- *   group of alphanumeric characters.
+ * - An array of strings, where each string represents an alphanumeric character
+ *   or group of alphanumeric characters.
  */
 export type CnpjInput = string | string[];
