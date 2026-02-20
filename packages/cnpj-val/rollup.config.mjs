3a352c2ed8ff8646ed206cc19777a1ad6dfb0af6
juliolmuller <juliolmuller@outlook.com>
2026-02-20 11:06:56 -0300

    feat(cnpj-val): define package entrypoints

--- packages/cnpj-val/rollup.config.mjs
+++ packages/cnpj-val/rollup.config.mjs
@@ -4,6 +4,9 @@ import packageMeta from './package.json' with { type: 'json' };
 export default makeRollupConfig({
   bannerTitle: 'Lacus Solutions :: cnpj-val',
   creationYear: 2021,
+  cjsEntryPoint: 'src/index.cjs.ts',
+  esmEntryPoint: 'src/index.esm.ts',
+  umdEntryPoint: 'src/index.umd.ts',
   umdGlobalName: 'cnpjVal',
   umdDistFileName: 'cnpj-val',
   packageMeta,
