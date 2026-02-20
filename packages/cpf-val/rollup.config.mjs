3a3f74df0e7e1d204214fec6a0b6adc8621f1614
juliolmuller <juliolmuller@outlook.com>
2026-02-20 14:31:09 -0300

    build(cpf-val): define external API and output files

--- packages/cpf-val/rollup.config.mjs
+++ packages/cpf-val/rollup.config.mjs
@@ -3,7 +3,10 @@ import packageMeta from './package.json' with { type: 'json' };
 
 export default makeRollupConfig({
   bannerTitle: 'Lacus Solutions :: cpf-val',
-  creationYear: 2020,
+  creationYear: 2021,
+  cjsEntryPoint: 'src/index.cjs.ts',
+  esmEntryPoint: 'src/index.esm.ts',
+  umdEntryPoint: 'src/index.umd.ts',
   umdGlobalName: 'cpfVal',
   umdDistFileName: 'cpf-val',
   packageMeta,
---
+++ packages/cpf-val/src/index.esm.ts
@@ -0,0 +1,6 @@
+export { cpfVal as default } from './cpf-val';
+
+export * from './cpf-val';
+export * from './cpf-validator';
+export * from './exceptions';
+export * from './types';
