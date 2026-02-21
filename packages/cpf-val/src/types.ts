7f743a9109dcb27d280ce6a09721ed758dc1999c
juliolmuller <juliolmuller@outlook.com>
2026-02-21 11:08:15 -0300

    docs(cpf-val): fix JSdocs
    Fix as per CodeRabbit AI review comment at https://github.com/LacusSolutions/br-utils-js/pull/53#discussion_r2836203932.

--- packages/cpf-val/src/types.ts
+++ packages/cpf-val/src/types.ts
@@ -3,7 +3,7 @@
  *
  * A CPF may be given as:
  *
- * - A string of alphanumeric characters (with or without formatting), or.
+ * - A string of numeric characters (with or without formatting).
  * - An array of strings, each representing one or more alphanumeric characters.
  */
 export type CpfInput = readonly string[] | string;
