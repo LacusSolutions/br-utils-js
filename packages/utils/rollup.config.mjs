e1af929670b8517763edfcb5531628726f83c7ea
juliolmuller <juliolmuller@outlook.com>
2026-02-23 09:12:41 -0300

    feat(utils): create project to maintain reusable utilities resources

---
+++ packages/utils/rollup.config.mjs
@@ -0,0 +1,13 @@
+import { makeRollupConfig } from '../../rollup.config.mjs';
+import packageMeta from './package.json' with { type: 'json' };
+
+export default makeRollupConfig({
+  bannerTitle: 'Lacus Solutions :: utils',
+  creationYear: 2026,
+  cjsEntryPoint: 'src/index.cjs.ts',
+  esmEntryPoint: 'src/index.esm.ts',
+  umdEntryPoint: 'src/index.umd.ts',
+  umdGlobalName: 'lacusUtils',
+  umdDistFileName: 'utils',
+  packageMeta,
+});
