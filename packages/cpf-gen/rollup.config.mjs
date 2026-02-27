cc5ad3d683ae5d8ef67c11e631ec60ed8a0a8150
juliolmuller <juliolmuller@outlook.com>
2026-02-26 21:40:22 -0300

    build(cpf-gen): update output API

--- packages/cpf-gen/rollup.config.mjs
+++ packages/cpf-gen/rollup.config.mjs
@@ -4,7 +4,6 @@ import packageMeta from './package.json' with { type: 'json' };
 export default makeRollupConfig({
   bannerTitle: 'Lacus Solutions :: cpf-gen',
   creationYear: 2021,
-  esmExports: 'named',
   umdGlobalName: 'cpfGen',
   umdDistFileName: 'cpf-gen',
   packageMeta,
--- packages/cpf-gen/src/index.cjs.ts
+++ packages/cpf-gen/src/index.cjs.ts
@@ -1,40 +1,7 @@
-import { cpfGen as baseCpfGen } from './cpf-gen';
-import { CpfGenerator as BaseCpfGenerator } from './cpf-generator';
-import {
-  CpfGeneratorOptions as BaseCpfGeneratorOptions,
-  CPF_LENGTH as BaseCpfLength,
-  CPF_PREFIX_MAX_LENGTH as BaseCpfPrefixMaxLength,
-} from './cpf-generator-options';
-import {
-  CpfGeneratorException as BaseCpfGeneratorException,
-  CpfGeneratorOptionPrefixInvalidException as BaseCpfGeneratorOptionPrefixInvalidException,
-  CpfGeneratorOptionsTypeError as BaseCpfGeneratorOptionsTypeError,
-  CpfGeneratorTypeError as BaseCpfGeneratorTypeError,
-} from './exceptions';
-import type {
-  CpfGeneratorOptionsInput as BaseCpfGeneratorOptionsInput,
-  CpfGeneratorOptionsType as BaseCpfGeneratorOptionsType,
-} from './types';
+import * as all from './index.esm';
 
-function cpfGen(options?: BaseCpfGeneratorOptionsInput): string {
-  return baseCpfGen(options);
-}
+const { default: _, cpfGen: baseCnpjFmt, ...rest } = all;
 
-namespace cpfGen {
-  // Runtime values (re-exported with original names)
-  export const CPF_LENGTH = BaseCpfLength;
-  export const CPF_PREFIX_MAX_LENGTH = BaseCpfPrefixMaxLength;
-  export const CpfGenerator = BaseCpfGenerator;
-  export const CpfGeneratorOptions = BaseCpfGeneratorOptions;
-  export const CpfGeneratorTypeError = BaseCpfGeneratorTypeError;
-  export const CpfGeneratorOptionsTypeError = BaseCpfGeneratorOptionsTypeError;
-  export const CpfGeneratorException = BaseCpfGeneratorException;
-  export const CpfGeneratorOptionPrefixInvalidException =
-    BaseCpfGeneratorOptionPrefixInvalidException;
+const cpfGen: typeof baseCnpjFmt = (...args) => baseCnpjFmt(...args);
 
-  // Type aliases
-  export type CpfGeneratorOptionsInput = BaseCpfGeneratorOptionsInput;
-  export type CpfGeneratorOptionsType = BaseCpfGeneratorOptionsType;
-}
-
-export default cpfGen;
+export default Object.assign(cpfGen, rest);
