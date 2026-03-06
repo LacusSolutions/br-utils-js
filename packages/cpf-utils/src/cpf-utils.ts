dc9ba7f8a836ce7b5199bc32b69b4dc9111b82e1
juliolmuller <juliolmuller@outlook.com>
2026-03-05 22:31:22 -0300

    docs(cpf-utils): fix JSdocs missing references
    Fix as per @coderabbitai review comment at https://github.com/LacusSolutions/br-utils-js/pull/65#discussion_r2891661769.

--- packages/cpf-utils/src/cpf-utils.ts
+++ packages/cpf-utils/src/cpf-utils.ts
@@ -10,6 +10,25 @@ import { CpfValidator, type CpfValidatorInput } from './cpf-validator';
 import { type CpfUtilsSettingsInput } from './types';
 
 /**
+ * @typedef {import('./cpf-formatter').CpfFormatterInputTypeError} CpfFormatterInputTypeError
+ *
+ *
+ * @typedef {import('./cpf-formatter').CpfFormatterOptionsTypeError} CpfFormatterOptionsTypeError
+ *
+ *
+ * @typedef {import('./cpf-formatter').CpfFormatterOptionsHiddenRangeInvalidException} CpfFormatterOptionsHiddenRangeInvalidException
+ *
+ *
+ * @typedef {import('./cpf-generator').CpfGeneratorOptionsTypeError} CpfGeneratorOptionsTypeError
+ *
+ *
+ * @typedef {import('./cpf-generator').CpfGeneratorOptionPrefixInvalidException} CpfGeneratorOptionPrefixInvalidException
+ *
+ *
+ * @typedef {import('./cpf-validator').CpfValidatorInputTypeError} CpfValidatorInputTypeError
+ */
+
+/**
  * Unified API for CPF (Cadastro da Pessoa Física) formatting, generation, and
  * validation. Wraps a configurable formatter, generator, and validator so you
  * can format, generate, and validate CPF values from a single instance.
@@ -32,12 +51,6 @@ export class CpfUtils {
    *   type.
    * @throws {CpfGeneratorOptionPrefixInvalidException} If generator `prefix` is
    *   invalid.
-   * @throws {CpfGeneratorOptionTypeInvalidException} If generator `type` is not
-   *   allowed.
-   * @throws {CpfValidatorOptionsTypeError} If validator options have an invalid
-   *   type.
-   * @throws {CpfValidatorOptionTypeInvalidException} If validator `type` is not
-   *   allowed.
    */
   public constructor(defaultSettings?: CpfUtilsSettingsInput) {
     this._formatter =
@@ -110,7 +123,6 @@ export class CpfUtils {
    *
    * @throws {CpfGeneratorOptionsTypeError} If options have an invalid type.
    * @throws {CpfGeneratorOptionPrefixInvalidException} If `prefix` is invalid.
-   * @throws {CpfGeneratorOptionTypeInvalidException} If `type` is not allowed.
    */
   public set generator(value: Nullable<CpfUtilsSettingsInput['generator']>) {
     this._generator = value instanceof CpfGenerator ? value : new CpfGenerator(value ?? undefined);
@@ -163,7 +175,6 @@ export class CpfUtils {
    *
    * @throws {CpfGeneratorOptionsTypeError} If any option has an invalid type.
    * @throws {CpfGeneratorOptionPrefixInvalidException} If `prefix` is invalid.
-   * @throws {CpfGeneratorOptionTypeInvalidException} If `type` is not allowed.
    */
   public generate(options?: CpfGeneratorOptionsInput): string {
     return this.generator.generate(options);
